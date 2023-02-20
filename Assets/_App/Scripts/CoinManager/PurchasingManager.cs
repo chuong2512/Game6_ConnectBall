72b6d2cf9b3fdb0eda5653c128e96ac7c8c17216
chuong2512 <hieutg244@gmail.com>
2023-02-20 22:47:26 +0700

    update id in app

--- Assets/_App/Scripts/CoinManager/PurchasingManager.cs
+++ Assets/_App/Scripts/CoinManager/PurchasingManager.cs
@@ -9,19 +9,23 @@ public class PurchasingManager : MonoBehaviour
       switch (i)
       {
          case 1:
-            GameDataManager.Instance.playerData.AddDiamond(1);
+            IAPManager.OnPurchaseSuccess = () => { GameDataManager.Instance.playerData.AddDiamond(1); };
+
              IAPManager.Instance.BuyProductID(IAPKey.PACK1);
             break;
          case 2:
-            GameDataManager.Instance.playerData.AddDiamond(2);
+            IAPManager.OnPurchaseSuccess = () => { GameDataManager.Instance.playerData.AddDiamond(2); };
+
             IAPManager.Instance.BuyProductID(IAPKey.PACK2);
             break;
          case 3:
-            GameDataManager.Instance.playerData.AddDiamond(5);
+            IAPManager.OnPurchaseSuccess = () => { GameDataManager.Instance.playerData.AddDiamond(5); };
+
             IAPManager.Instance.BuyProductID(IAPKey.PACK3);
             break;
          case 4:
-            GameDataManager.Instance.playerData.AddDiamond(10);
+            IAPManager.OnPurchaseSuccess = () => { GameDataManager.Instance.playerData.AddDiamond(10); };
+
             IAPManager.Instance.BuyProductID(IAPKey.PACK4);
             break;
       }
